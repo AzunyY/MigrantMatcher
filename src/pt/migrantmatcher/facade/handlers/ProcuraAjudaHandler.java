1122c12c74beb93b185e62dbc23dcf3ce963abdc
Catarina <azunynyan@hotmail.com>
2022-05-29 05:50:05 +0100

    started usecase2

--- src/pt/migrantmatcher/facade/handlers/ProcuraAjudaHandler.java
+++ src/pt/migrantmatcher/facade/handlers/ProcuraAjudaHandler.java
@@ -1,10 +1,49 @@
 package pt.migrantmatcher.facade.handlers;
 
+import pt.migrantmatcher.domain.CatalogoMigrantes;
+
 public class ProcuraAjudaHandler {
 	
+	private CatalogoMigrantes catMigrantes;
 	
 	public ProcuraAjudaHandler() {
+		catMigrantes = new CatalaogoMigrantes();
+	}
+	
+	public void iniciaRegistoPessoas(String nome, int tel) {
 		
 	}
 	
+	public void iniciaRegistoFamilia(int nPessoas) {
+		
+	}
+	
+	public void infoCabeça(String nome, int tel) {
+		
+	}
+	
+	public void indicaInfoFamiliar(String nome) {
+		
+	}
+	
+	public List <Regiao> pedeListaRegioes(){
+		
+	}
+	
+	public List <Ajuda> indicaRegiao() {
+		
+	}
+	
+	public void escolheAjuda() {
+		
+	}
+	
+	public void confirmaRegisto() {
+		
+	}
+	
+	/*Possivel que seja com observavel*/
+	public void pedeNotifAjuda(Regiao reg) {
+		
+	}
 }
